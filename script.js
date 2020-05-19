677f5c7ed06181e7602264d566ac143f2a12816f
phardwoodward <paul_january10@yahoo.com>
2020-05-19 18:27:14 +0000

    a couple changes has been made

---
+++ script.js
@@ -0,0 +1,18 @@
+/* this external script file is the javascript file to calculate the 
+ the loans for the index*/
+
+ function loanProcess(){
+     //this is the main function
+
+     var 
+ }
+
+function add(){
+
+    var one;
+    var two;
+    var sum;
+
+    sum = one + two;
+
+}
