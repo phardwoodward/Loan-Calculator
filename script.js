5278ef3e8f7025d9ce51b3a89e65ea13b85a97f5
phardwoodward <paul_january10@yahoo.com>
2020-05-20 14:02:42 +0000

    external script file is now functionable

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 /* this external script file is the javascript file to calculate the 
  the loans for the index*/
 
- function loanProcess(){
-     //this is the main function
-     var loan = document.getElementById("applyLoan");
-     var text = "your num1 is: " + num1 + " your num2 is "
-     + num2 "\n" +
-     "add them together " + sum;
-    
-     
- }//loanProcess
-
-function add(){
-
-    var one;
-    var two;
-    var sum;
-
-    alert(sum);
-}//add
-
-function displayDetails(){
+function loanProcess(){
 
 
-}//displayDetails
+	var one = document.getElementById("num1").value;
+    var two = document.getElementById("num2").value;
+    var sum = parseInt(one) + parseInt(two);
+        //alert(sum);
 
+        document.getElementById("answer").innerHTML = sum;
+        }
